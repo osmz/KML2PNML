970cf134f82cfdd09d19db120f7e7d04081eeb78
Oscar Morales <oscar.s.morales.z@usp.br>
2022-07-27 12:30:55 -0500

    Modify funtionCalculateQuantifyTransition.py

--- funtionCalculateQuantifyTransition.py
+++ funtionCalculateQuantifyTransition.py
@@ -1,4 +1,4 @@
-def calculate_Quantify_Transition(ToRefineAnd, ToRefineOr, list_Vector, list_Connection, list_Transition):
+def calculate_Quantify_Transition(ToRefineAnd, ToRefineOr, input_Vector_List, list_Connection, list_Transition, list_Goal, output_Vector_List):
     cont_Transition = 0
     for index in range(len(ToRefineAnd)):
         if ToRefineAnd[index] != 'NONE' and ToRefineOr[index] == 'NONE':
@@ -6,9 +6,10 @@ def calculate_Quantify_Transition(ToRefineAnd, ToRefineOr, list_Vector, list_Con
             separador = ';'
             separador = cadena.split(separador)
             for index_Separador_And in range(len(separador)):
-                list_Vector.append(separador[index_Separador_And])
+                input_Vector_List.append(separador[index_Separador_And])
                 list_Connection.append('And')
                 list_Transition.append(cont_Transition)
+                output_Vector_List.append(list_Goal[index].place_Id())
             cont_Transition += 1
         
         elif ToRefineOr[index] != 'NONE' and ToRefineAnd[index] == 'NONE':
@@ -16,9 +17,10 @@ def calculate_Quantify_Transition(ToRefineAnd, ToRefineOr, list_Vector, list_Con
             separador = ';'
             separador = cadena.split(separador)
             for index_Separador_Or in range(len(separador)):
-                list_Vector.append(separador[index_Separador_Or])
+                input_Vector_List.append(separador[index_Separador_Or])
                 list_Connection.append('Or')
                 list_Transition.append(cont_Transition)
+                output_Vector_List.append(list_Goal[index].place_Id())
                 cont_Transition += 1    
 
         elif ToRefineAnd[index] != 'NONE' and ToRefineOr[index] != 'NONE':
@@ -27,9 +29,10 @@ def calculate_Quantify_Transition(ToRefineAnd, ToRefineOr, list_Vector, list_Con
                 separador = ';'
                 separador = cadena.split(separador)
                 for index_Separador_And_1 in range(len(separador)):
-                    list_Vector.append(separador[index_Separador_And_1])
+                    input_Vector_List.append(separador[index_Separador_And_1])
                     list_Connection.append('And')
                     list_Transition.append(cont_Transition)
+                    output_Vector_List.append(list_Goal[index].place_Id())
                 cont_Transition += 1
             
             if ToRefineOr[index] != 'NONE':
@@ -37,10 +40,11 @@ def calculate_Quantify_Transition(ToRefineAnd, ToRefineOr, list_Vector, list_Con
                 separador = ';'
                 separador = cadena.split(separador)
                 for index_Separador_Or_1 in range(len(separador)):
-                    list_Vector.append(separador[index_Separador_Or_1])
+                    input_Vector_List.append(separador[index_Separador_Or_1])
                     list_Connection.append('Or')
                     list_Transition.append(cont_Transition)
-                    cont_Transition += 1
+                    output_Vector_List.append(list_Goal[index].place_Id())
+                    cont_Transition += 1  
     
     return cont_Transition
     
