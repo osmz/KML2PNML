dd51e5534cb44d8b2d95acee9c87e3542f661e54
Oscar Morales <oscar.s.morales.z@usp.br>
2022-07-27 12:32:31 -0500

    Modify funtionDrawArrowOut.py

--- funtionDrawArrowOut.py
+++ funtionDrawArrowOut.py
@@ -1,19 +1,15 @@
 import xml.etree.ElementTree as ET
 
-def draw_Arrow_Out(list_Type, ToRefineAnd, ToRefineOr, index, net, list_Vector, list_Transition, list_Goal):
-    if list_Type == 'ToRefineAnd':
-        cadena = ToRefineAnd[index]
-        separador = ';'
-        separador = cadena.split(separador)
-        indice_1 = list_Vector.index(str(separador[0]))
-        indice_2 = ToRefineAnd.index(cadena)
-
+def draw_Arrow_Out(list_Transition, index, net, output_Vector_List):
+    if list_Transition[index - 1 ] != list_Transition[index]:
         # Arc 1
         # Son 1.4
         arc = ET.SubElement(net, 'arc') 
-        arc.set('id', 'T' + str(list_Transition[indice_1]) + ' to ' + list_Goal[indice_2].place_Id())
-        arc.set('source', 'T' + str(list_Transition[indice_1]))
-        arc.set('target', list_Goal[indice_2].place_Id())
+        arc.set('id', 'T' + str(list_Transition[index]) + ' to ' + output_Vector_List[index])
+        ''' print('position teste and out')
+        print('T' + str(list_Transition[indice_1]) + ' to ' + list_Goal[indice_2].place_Id()) '''
+        arc.set('source', 'T' + str(list_Transition[index]))
+        arc.set('target', output_Vector_List[index])
 
         # Son 1.4.1
         element1 = ET.SubElement(arc, 'graphics')
@@ -49,60 +45,3 @@ def draw_Arrow_Out(list_Type, ToRefineAnd, ToRefineOr, index, net, list_Vector, 
         # Son 1.4.6
         element6 = ET.SubElement(arc, 'type')
         element6.set('value', 'normal')
-
-    elif list_Type == 'ToRefineOr':
-        cadena = ToRefineOr[index]
-        separador = ';'
-        separador = cadena.split(separador)
-        
-        for aux_Separador in range(len(separador)):
-            indice_1 = list_Vector.index(str(separador[aux_Separador]))
-            indice_2 = ToRefineOr.index(cadena)
-
-            # Arc 1
-            # Son 1.4
-            arc = ET.SubElement(net, 'arc') 
-            arc.set('id', 'T' + str(list_Transition[indice_1]) + ' to ' + list_Goal[indice_2].place_Id())
-            arc.set('source', 'T' + str(list_Transition[indice_1]))
-            arc.set('target', list_Goal[indice_2].place_Id())
-
-            # Son 1.4.1
-            element1 = ET.SubElement(arc, 'graphics')
-
-            # Son 1.4.2
-            element2 = ET.SubElement(arc, 'inscription')
-            # Son 1.4.2.1
-            sub_element2 = ET.SubElement(element2, 'value') 
-            sub_element2.text = 'Default,' + '1'
-            # Son 1.4.2.2
-            sub_element2 = ET.SubElement(element2, 'graphics')
-
-            # Son 1.4.3
-            element3 = ET.SubElement(arc, 'tagged')
-            # Son 1.4.3.1 
-            sub_element3 = ET.SubElement(element3, 'value') 
-            sub_element3.text = 'false'
-
-            # Son 1.4.4
-            element4 = ET.SubElement(arc, 'arcpath')
-            element4.set('id', '000')
-            element4.set('x', '0')
-            element4.set('y', '0')
-            element4.set('curvePoint', 'false')
-
-            # Son 1.4.5
-            element5 = ET.SubElement(arc, 'arcpath')
-            element5.set('id', '001')
-            element5.set('x', '0')
-            element5.set('y', '0')
-            element5.set('curvePoint', 'false')
-
-            # Son 1.4.6
-            element6 = ET.SubElement(arc, 'type')
-            element6.set('value', 'normal')
-
-            
-        
-        
-    
-    
