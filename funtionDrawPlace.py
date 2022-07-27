0336bb9778069d58bd32096174aa74857a7f9c4f
Oscar Morales <oscar.s.morales.z@usp.br>
2022-07-27 12:33:02 -0500

    Modify funtionDrawPlace.py

--- funtionDrawPlace.py
+++ funtionDrawPlace.py
@@ -1,9 +1,6 @@
 import xml.etree.ElementTree as ET
 
-def draw_Place(net, list_Goal, index, starting_Position_X_Place, starting_Position_Y_Place):
-    # Convert the positions to string
-    starting_Position_X_Place = str(starting_Position_X_Place)
-    starting_Position_Y_Place = str(starting_Position_Y_Place)
+def draw_Place(net, list_Goal, index):
 
     # Place
     # Son 1.2
@@ -13,9 +10,9 @@ def draw_Place(net, list_Goal, index, starting_Position_X_Place, starting_Positi
     # Son 1.2.1
     element1 = ET.SubElement(place, 'graphics')
     # Son 1.2.1.1 
-    sub_element1 = ET.SubElement(element1, 'position') 
-    sub_element1.set('x', starting_Position_X_Place) 
-    sub_element1.set('y', starting_Position_Y_Place) 
+    sub_element1 = ET.SubElement(element1, 'position')
+    sub_element1.set('x', list_Goal[index].place_PositionX()) 
+    sub_element1.set('y', list_Goal[index].place_PositionY()) 
 
     # Son 1.2.2
     element2 = ET.SubElement(place, 'name')
@@ -46,9 +43,5 @@ def draw_Place(net, list_Goal, index, starting_Position_X_Place, starting_Positi
     # Son 1.2.4.1
     sub_element4 = ET.SubElement(element4, 'value')
     sub_element4.text = '0'
-
-    # Convert the positions to float
-    starting_Position_X_Place = float(starting_Position_X_Place)
-    starting_Position_Y_Place = float(starting_Position_Y_Place)
 
     
