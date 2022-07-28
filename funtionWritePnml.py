c1624929e3ca431fdf0b25a7851053445934a3fb
Oscar Morales <oscar.s.morales.z@usp.br>
2022-07-28 10:13:20 -0500

    Modify funtionWritePnml.py

--- funtionWritePnml.py
+++ funtionWritePnml.py
@@ -5,7 +5,7 @@ import funtionDrawTransition as DTransition
 import funtionDrawArrowOut as DArrowOut
 import funtionDrawArrowInt as DArrowInt
 
-def write_Pnml(helper_For_Goal_Size, list_Goal, quant_Transition, input_Vector_List, list_Transition, output_Vector_List):
+def write_Pnml(helper_For_Goal_Size, list_Goal, quant_Transition, input_Vector_List, list_Transition, output_Vector_List, Id):
     # Father of the network - has children
     pnml = ET.Element('pnml')
 
@@ -25,18 +25,10 @@ def write_Pnml(helper_For_Goal_Size, list_Goal, quant_Transition, input_Vector_L
     # For to create all elements Place
     for index in range(helper_For_Goal_Size - 1, -1, -1):
         DPlace.draw_Place(net, list_Goal, index)
-    
-    # Initialize the position variables of the Transition
-    starting_Position_X_Transition = 400.0
-    starting_Position_Y_Transition = 200.0 
 
     # For to create all elements Transition
     for index in range(quant_Transition):
-        DTransition.draw_Transition(net, index, starting_Position_X_Transition, starting_Position_Y_Transition)
-
-        # Increase the positions
-        starting_Position_X_Transition += 50.0
-        starting_Position_Y_Transition += 50.0
+        DTransition.draw_Transition(net, index, input_Vector_List, output_Vector_List, Id, list_Goal, list_Transition)
     
     # For to create all elements Arc
     for index in range(len(input_Vector_List)): 
