311d009f710631100e149ff2458cb8eb9f850088
Oscar Morales <oscar.s.morales.z@usp.br>
2022-09-16 18:49:02 -0500

    Modify file to include the @ separation and then the ;

--- funtionCalculateQuantifyTransition.py
+++ funtionCalculateQuantifyTransition.py
@@ -4,16 +4,32 @@ def calculate_Quantify_Transition(Id, ToRefineAnd, ToRefineOr, input_Vector_List
     for index in range(len(ToRefineAnd)):
         if ToRefineAnd[index] != 'NONE' and ToRefineOr[index] == 'NONE':
             cadena = ToRefineAnd[index]
-            separador = ';'
+            separador = '@'
             separador = cadena.split(separador)
-            for index_Separador_And in range(len(separador)):
-                input_Vector_List.append(separador[index_Separador_And])
-                list_Connection.append('And')
-                indice = lst.index(separador[index_Separador_And])
-                list_Type.append(list_Goal[indice].goal_Type())
-                list_Transition.append(cont_Transition)
-                output_Vector_List.append(list_Goal[index].goal_Id())
-            cont_Transition += 1
+            for index_Separador_And_Arroa in range(len(separador)):
+                cadena = separador[index_Separador_And_Arroa]
+                separador2 = ';'
+                separador2 = cadena.split(separador2)
+                for index_Separador_And_PuntoYComa in range(len(separador2)):
+                    input_Vector_List.append(separador2[index_Separador_And_PuntoYComa])
+                    list_Connection.append('And')
+                    indice = lst.index(separador2[index_Separador_And_PuntoYComa])
+                    list_Type.append(list_Goal[indice].goal_Type())
+                    list_Transition.append(cont_Transition)
+                    output_Vector_List.append(list_Goal[index].goal_Id())
+                cont_Transition += 1
+
+            #cadena = ToRefineAnd[index]
+            #separador = ';'
+            #separador = cadena.split(separador)
+            #for index_Separador_And in range(len(separador)):
+            #    input_Vector_List.append(separador[index_Separador_And])
+            #    list_Connection.append('And')
+            #    indice = lst.index(separador[index_Separador_And])
+            #    list_Type.append(list_Goal[indice].goal_Type())
+            #    list_Transition.append(cont_Transition)
+            #    output_Vector_List.append(list_Goal[index].goal_Id())
+            #cont_Transition += 1
         
         elif ToRefineAnd[index] == 'NONE' and ToRefineOr[index] != 'NONE':
             cadena = ToRefineOr[index]
@@ -31,16 +47,32 @@ def calculate_Quantify_Transition(Id, ToRefineAnd, ToRefineOr, input_Vector_List
         elif ToRefineAnd[index] != 'NONE' and ToRefineOr[index] != 'NONE':
             if ToRefineAnd[index] != 'NONE':
                 cadena = ToRefineAnd[index]
-                separador = ';'
+                separador = '@'
                 separador = cadena.split(separador)
-                for index_Separador_And_1 in range(len(separador)):
-                    input_Vector_List.append(separador[index_Separador_And_1])
-                    list_Connection.append('And')
-                    indice = lst.index(separador[index_Separador_And_1])
-                    list_Type.append(list_Goal[indice].goal_Type())
-                    list_Transition.append(cont_Transition)
-                    output_Vector_List.append(list_Goal[index].goal_Id())
-                cont_Transition += 1
+                for index_Separador_And_Arroa in range(len(separador)):
+                    cadena = separador[index_Separador_And_Arroa]
+                    separador2 = ';'
+                    separador2 = cadena.split(separador2)
+                    for index_Separador_And_PuntoYComa in range(len(separador2)):
+                        input_Vector_List.append(separador2[index_Separador_And_PuntoYComa])
+                        list_Connection.append('And')
+                        indice = lst.index(separador2[index_Separador_And_PuntoYComa])
+                        list_Type.append(list_Goal[indice].goal_Type())
+                        list_Transition.append(cont_Transition)
+                        output_Vector_List.append(list_Goal[index].goal_Id())
+                    cont_Transition += 1
+
+                #cadena = ToRefineAnd[index]
+                #separador = ';'
+                #separador = cadena.split(separador)
+                #for index_Separador_And_1 in range(len(separador)):
+                #    input_Vector_List.append(separador[index_Separador_And_1])
+                #    list_Connection.append('And')
+                #    indice = lst.index(separador[index_Separador_And_1])
+                #    list_Type.append(list_Goal[indice].goal_Type())
+                #    list_Transition.append(cont_Transition)
+                #    output_Vector_List.append(list_Goal[index].goal_Id())
+                #cont_Transition += 1
             
             if ToRefineOr[index] != 'NONE':
                 cadena = ToRefineOr[index]
