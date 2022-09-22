3ca5a9912da2014d2bf06e8c04e7fcc5e3af348b
Oscar Morales <oscar.s.morales.z@usp.br>
2022-09-22 10:44:22 -0500

    Modify funtionDrawArrowOut.py file

--- funtionDrawArrowOut.py
+++ funtionDrawArrowOut.py
@@ -1,47 +1,46 @@
 import xml.etree.ElementTree as ET
 
-def draw_Arrow_Out(list_Transition, index, net, output_Vector_List):
-    if list_Transition[index - 1 ] != list_Transition[index]:
-        # Arc 1
-        # Son 1.4
-        arc = ET.SubElement(net, 'arc') 
-        arc.set('id', 'T' + str(list_Transition[index]) + ' to ' + output_Vector_List[index])
-        ''' print('position teste and out')
-        print('T' + str(list_Transition[indice_1]) + ' to ' + list_Goal[indice_2].place_Id()) '''
-        arc.set('source', 'T' + str(list_Transition[index]))
-        arc.set('target', output_Vector_List[index])
+def draw_Arrow_Out(net, A_name_T, A_cont_name_T, A_name_id):
+    # Arc 1
+    # Son 1.4
+    arc = ET.SubElement(net, 'arc') 
+    arc.set('id', 'T' + str(A_name_T[A_cont_name_T]) + ' to ' + A_name_id[A_cont_name_T])
+    ''' print('position teste and out')
+    print('T' + str(A_name_T[A_cont_name_T]) + ' to ' + A_name_id[A_cont_name_T]) '''
+    arc.set('source', 'T' + str(A_name_T[A_cont_name_T]))
+    arc.set('target', A_name_id[A_cont_name_T])
 
-        # Son 1.4.1
-        element1 = ET.SubElement(arc, 'graphics')
+    # Son 1.4.1
+    element1 = ET.SubElement(arc, 'graphics')
 
-        # Son 1.4.2
-        element2 = ET.SubElement(arc, 'inscription')
-        # Son 1.4.2.1
-        sub_element2 = ET.SubElement(element2, 'value') 
-        sub_element2.text = 'Default,' + '1'
-        # Son 1.4.2.2
-        sub_element2 = ET.SubElement(element2, 'graphics')
+    # Son 1.4.2
+    element2 = ET.SubElement(arc, 'inscription')
+    # Son 1.4.2.1
+    sub_element2 = ET.SubElement(element2, 'value') 
+    sub_element2.text = 'Default,' + '1'
+    # Son 1.4.2.2
+    sub_element2 = ET.SubElement(element2, 'graphics')
 
-        # Son 1.4.3
-        element3 = ET.SubElement(arc, 'tagged')
-        # Son 1.4.3.1 
-        sub_element3 = ET.SubElement(element3, 'value') 
-        sub_element3.text = 'false'
+    # Son 1.4.3
+    element3 = ET.SubElement(arc, 'tagged')
+    # Son 1.4.3.1 
+    sub_element3 = ET.SubElement(element3, 'value') 
+    sub_element3.text = 'false'
 
-        # Son 1.4.4
-        element4 = ET.SubElement(arc, 'arcpath')
-        element4.set('id', '000')
-        element4.set('x', '0')
-        element4.set('y', '0')
-        element4.set('curvePoint', 'false')
+    # Son 1.4.4
+    element4 = ET.SubElement(arc, 'arcpath')
+    element4.set('id', '000')
+    element4.set('x', '0')
+    element4.set('y', '0')
+    element4.set('curvePoint', 'false')
 
-        # Son 1.4.5
-        element5 = ET.SubElement(arc, 'arcpath')
-        element5.set('id', '001')
-        element5.set('x', '0')
-        element5.set('y', '0')
-        element5.set('curvePoint', 'false')
+    # Son 1.4.5
+    element5 = ET.SubElement(arc, 'arcpath')
+    element5.set('id', '001')
+    element5.set('x', '0')
+    element5.set('y', '0')
+    element5.set('curvePoint', 'false')
 
-        # Son 1.4.6
-        element6 = ET.SubElement(arc, 'type')
-        element6.set('value', 'normal')
+    # Son 1.4.6
+    element6 = ET.SubElement(arc, 'type')
+    element6.set('value', 'normal')
