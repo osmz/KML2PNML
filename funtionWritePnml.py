90d5901c1282c71c72dd3be99810919bcd42e114
Oscar Morales <oscar.s.morales.z@usp.br>
2022-09-22 10:45:06 -0500

    Modify funtionWritePnml.py file

--- funtionWritePnml.py
+++ funtionWritePnml.py
@@ -1,13 +1,14 @@
 import xml.etree.ElementTree as ET
 
 import funtionDrawPlace as DPlace
-import funtionDrawTransition as DTransition
+import funtionCalculateTransitionPosition as CTransitionPosition
+import funtionDrawTransition as DTransiton
 import funtionDrawArrowOut as DArrowOut
 import funtionDrawArrowInt as DArrowInt
 import funtionDrawExpectation as DrawExpectation
-import funtionDrawOperationalization as DrawOperationalization
+import funtionDrawOperationalization2 as DrawOperationalization2
 
-def write_Pnml(helper_For_Goal_Size, list_Goal, quant_Transition, input_Vector_List, output_Vector_List, Id, list_Transition, ToRefineAnd, ToRefineOr, ExpectationOf, OperationalizationOf, list_Operation):
+def write_Pnml(helper_For_Goal_Size, list_Goal, cont_Transition, input_Vector_List, output_Vector_List, list_Transition, lst_Id_Goal, ToRefineAnd, ToRefineOr, ExpectationOf, OperationalizationOf, list_Operation, list_Connection, list_Type, Id, lst_Id_Operation):
     # Father of the network - has children
     pnml = ET.Element('pnml')
 
@@ -24,29 +25,44 @@ def write_Pnml(helper_For_Goal_Size, list_Goal, quant_Transition, input_Vector_L
     token.set('green', '0')
     token.set('blue', '0')
 
-    # For to create all elements goal
+    # For to create all elements Place
     for index in range(helper_For_Goal_Size - 1, -1, -1):
-        DPlace.draw_Place(net, list_Goal, index)
+        P_name = list_Goal[index].goal_Id()
+        P_position_X = list_Goal[index].goal_PositionX()
+        P_position_Y = list_Goal[index].goal_PositionY()
+        DPlace.draw_Place(net, P_name, P_position_X, P_position_Y)
 
     # For to create all elements Transition
-    for index in range(quant_Transition):
-        DTransition.draw_Transition(net, index, input_Vector_List, output_Vector_List, Id, list_Goal, list_Transition)
+    for index in range(cont_Transition):
+        (indice_Final_X, indice_Final_Y) = CTransitionPosition.calculate_Transition_Position(index, list_Transition, input_Vector_List, output_Vector_List, lst_Id_Goal, list_Goal)
+        T_name = index
+        T_position_X = indice_Final_X
+        T_position_Y = indice_Final_Y
+        DTransiton.draw_Transition(net, T_name, T_position_X, T_position_Y)
+        
+    # Create all Expectation
+    DrawExpectation.draw_Expectation(input_Vector_List, ToRefineAnd, ToRefineOr, ExpectationOf, list_Transition, net)
+    
+    # Create all Operationalization
+    DrawOperationalization2.draw_Operationalization(cont_Transition, OperationalizationOf, lst_Id_Operation, list_Operation, net, input_Vector_List, list_Connection, list_Type, list_Transition, output_Vector_List, Id)
     
     # For to create all elements Arc
     for index in range(len(input_Vector_List)): 
-        DArrowOut.draw_Arrow_Out(list_Transition, index, net, output_Vector_List)
-        #DArrowInt.draw_Arrow_Int(net, input_Vector_List, index, list_Transition)
-        A_normal = input_Vector_List
-        cont_1_normal = index
-        B_normal = list_Transition
-        cont_2_normal = index
-        
-        DArrowInt.draw_Arrow_Int(net, A_normal, cont_1_normal, B_normal, cont_2_normal)
-    
-    # Create all Expectation
-    DrawExpectation.draw_Expectation(ToRefineAnd, ToRefineOr, ExpectationOf, input_Vector_List, net, list_Transition)
+        A_name_T = list_Transition
+        A_cont_name_T = index
+        A_name_id = output_Vector_List
 
-    # Create all Operationalization
-    DrawOperationalization.draw_Operationalization(OperationalizationOf, list_Transition, Id, list_Operation, net)
+        if len(list_Transition) == 1:
+            DArrowOut.draw_Arrow_Out(net, A_name_T, A_cont_name_T, A_name_id)
+        elif len(list_Transition) > 1:
+            if list_Transition[index - 1 ] != list_Transition[index]:
+                DArrowOut.draw_Arrow_Out(net, A_name_T, A_cont_name_T, A_name_id)
+
+        A_name_id = input_Vector_List
+        A_cont_name_id = index
+        A_name_T = list_Transition
+        A_cont_name_T = index
+        
+        DArrowInt.draw_Arrow_Int(net, A_name_id, A_cont_name_id, A_name_T, A_cont_name_T) 
 
     return pnml
